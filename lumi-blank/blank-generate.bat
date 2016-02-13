703e899a19b32866dabb54370068d472139d408d
A-pZ <a-pz@h4.dion.ne.jp>
2016-02-13 17:26:36 +0900

    インストールバッチを更新。4.1.0

--- lumi-blank/blank-generate.bat
+++ lumi-blank/blank-generate.bat
@@ -1,3 +1,3 @@
-mvn archetype:generate -DarchetypeGroupId=serendip -DarchetypeArtifactId=lumi-blank -DarchetypeVersion=4.0.0 -DgroupId=lumi.sample -DartifactId=appname -Dversion=1.0.0 -DinteractiveMode=false
+mvn archetype:generate -DarchetypeGroupId=serendip -DarchetypeArtifactId=lumi-blank -DarchetypeVersion=4.1.0 -DgroupId=lumi.sample -DartifactId=appname -Dversion=1.0.0 -DinteractiveMode=false
 
 pause
