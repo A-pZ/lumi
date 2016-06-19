f6e3b390ac9f660683a412ea7fea4a12a4526134
A-pZ <a-pz@h4.dion.ne.jp>
2016-06-19 21:42:36 +0900

    Update Struts2-Thymeleaf3-plguin (#58)
    * version4.1.0 pom.xml
    
    * test case add.
    
    * BigDecimalCOnverterTestを追加
    
    * BigDecimalConverterTestを追加
    
    * Issue #45 アノテーションパラメータインターセプタの適用
    
    * #47 Advise系のリファクタリング
    
    * Actionクラスの修正
    
    * #47 Advise系のリファクタリング
    
    * Actionクラスの修正
    * gitignoreの修正
    
    * テストパッケージのエンコーディング設定を追記(eclipse内部ファイル)
    
    * MockAction/MockServiceのコミット
    
    * Issue #50 AbstractAdviseのログ出力改善
    
    * ログ出力するときのオブジェクトサイズが32kを越えるものは出力しない
    
    * 4.1.0 コミット漏れを修正
    
    * wst componentの更新
    
    * インストールバッチを更新。4.1.0
    
    * Struts2.5-BETA3に修正
    
    * read.me修正
    
    インストール方法を明記。batファイルの利用手順を追加。
    
    * * skiptestを追加
    
    * Update README.md
    
    * Fix version 2.3.28 and Java8 (#53)
    
    * fix for Struts2.3.28 and Java8
    * lumi-blank add serviet-api.3.0.1
    
    * ブランクプロジェクトバージョンの修正
    
    * Ignoreの追加
    
    * Lumi5.0.0 
    
    * Struts 2.5
    
    * Struts2.5動作検証済み
    
    不要なファイル削除
    
    * update eclipse setting
    
    * Update version 2.5.1 (#57)
    
    * Thymeleaf3 version update
    
    * Spring4.2.xsdへ変更
    
    * Struts2-Thymeleaf3 version対応
    
    * ignore修正
    
    * gitigonore更新
    
    * gitignore更新
    
    * Update Struts2 version 2.5.1

--- lumi-core/src/main/java/lumi/di/AbstractAdvise.java
+++ lumi-core/src/main/java/lumi/di/AbstractAdvise.java
@@ -1,3 +1,41 @@
+<<<<<<< HEAD
+/**
+ *
+ */
+package lumi.di;
+
+import org.aspectj.lang.JoinPoint;
+
+import lombok.extern.log4j.Log4j2;
+
+/**
+ * LumiのAdvise抽象クラス。
+ * @author A-pZ ( Serendipity 3 ./ as sundome goes by. )
+ *
+ */
+@Log4j2
+public abstract class AbstractAdvise {
+
+	/**
+	 * 引数オブジェクトのデバッグログを出力する。
+	 *
+	 * @param joinPoint
+	 *            ポイントカットしたオブジェクト
+	 */
+	protected void trace(JoinPoint joinPoint) {
+		if (! log.isDebugEnabled()) {
+			return;
+		}
+
+		Object[] objs = joinPoint.getArgs();
+		if (objs == null || objs.length == 0) {
+			return;
+		}
+
+		log.debug("-- arguments[{}]" , objs.length);
+	}
+}
+=======
 /**
  *
  */
@@ -58,3 +96,4 @@ public abstract class AbstractAdvise {
 		return baos.size();
 	}
 }
+>>>>>>> refs/remotes/origin/master
